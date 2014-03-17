e0689e36928b5302d17343d09b1bcfd56f067488
Matteo Ferrando <matteo.ferrando2@gmail.com>
2014-03-17 12:51:55 -0430

    linking instead of rsync

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,24 +5,43 @@ cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
 
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
-	source ~/.bash_profile
+    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+        --exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
+    source ~/.bash_profile
 }
 
+function removeIt() {
+    rm ~/.aliases ~/.bash_profile ~/.bash_prompt ~/.bashrc ~/.exports\
+        ~/.functions ~/.gemrc ~/.gitconfig ~/.gitignore ~/.iftoprc ~/.inputrc ~/.wgetrc
+}
 
 function linkIt() {
-    ln -s
+    ln -s .aliases      ~/.aliases
+    ln -s .bash_profile ~/.bash_profile
+    ln -s .bash_prompt  ~/.bash_prompt
+    ln -s .bashrc       ~/.bashrc
+    ln -s .exports      ~/.exports
+    ln -s .functions    ~/.functions
+    ln -s .gemrc        ~/.gemrc
+    ln -s .gitconfig    ~/.gitconfig
+    ln -s .gitignore    ~/.gitignore
+    ln -s .iftoprc      ~/.iftoprc
+    ln -s .inputrc      ~/.inputrc
+    ln -s .wgetrc       ~/.wgetrc
 }
 
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
+	removeIt
+	linkIt
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
+		removeIt
+		linkIt
 	fi
 fi
 unset doIt
+unset linkIt
+unset removeIt
