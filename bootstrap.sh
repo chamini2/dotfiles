21574afc6ab30067e9303cc86bc56fbb63487f6d
Matteo Ferrando <matteo.ferrando2@gmail.com>
2014-08-20 18:40:43 -0430

    Now it checks before creating the .cabal directory Now making cdargs based on system

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,10 +25,33 @@ function moveIt() {
     source ~/.bash_profile
 }
 
+# cdargs
+function prepare_cdargs() {
+    cat ~/.cdargs | sponge "$PWD/.cdargs"
+
+    echo "repos" "`echo ~`/Projects/"    >> "$PWD/.cdargs"
+    echo "db"    "`echo ~`/Dropbox/"     >> "$PWD/.cdargs"
+    echo "usb"   "`echo ~`/Dropbox/USB/" >> "$PWD/.cdargs"
+
+    sort "$PWD/.cdargs" -u -o "$PWD/.cdargs"
+}
+
 function removeIt() {
-    rm ~/.aliases ~/.bash_profile ~/.bash_prompt ~/.bashrc ~/.exports\
-        ~/.functions ~/.gemrc ~/.gitconfig ~/.gitignore ~/.iftoprc\
-        ~/.inputrc ~/.wgetrc ~/.cabal/config
+    rm ~/.aliases
+    rm ~/.bash_profile
+    rm ~/.bash_prompt
+    rm ~/.bashrc
+    rm ~/.cdargs
+    rm ~/.exports
+    rm ~/.functions
+    rm ~/.gemrc
+    rm ~/.gitconfig
+    rm ~/.gitignore
+    rm ~/.iftoprc
+    rm ~/.inputrc
+    rm ~/.wgetrc
+
+    rm ~/.cabal/config
 }
 
 function linkIt() {
@@ -44,26 +67,29 @@ function linkIt() {
     ln -s "$PWD/.iftoprc"      ~/.iftoprc
     ln -s "$PWD/.inputrc"      ~/.inputrc
     ln -s "$PWD/.wgetrc"       ~/.wgetrc
-    cp    "$PWD/.gitconfig"    ~/.gitconfig   # We don't want to put email and name in the repo
-    
-    mkdir ~/.cabal
-    ln -s "$PWD/cabalconfig"   ~/.cabal/config
 
+    cp    "$PWD/.gitconfig"    ~/.gitconfig   # We don't want to put email and name in the repo
+
+    if ! [ -d ~/.cabal ]; then
+        mkdir ~/.cabal
+    fi
+    ln -s "$PWD/.cabal/config"   ~/.cabal/config
 }
 
 function doIt() {
-	removeIt
-	linkIt
+    prepare_cdargs
+    removeIt
+    linkIt
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
+    doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doIt
+    fi
 fi
 unset moveIt
 unset linkIt
